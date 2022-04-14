a1aff546753078df87bdc47b8123b3b9742b717b
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-04-14 14:49:03 -0700

    ignore

--- sbi_testing.R
+++ sbi_testing.R
@@ -13,3 +13,13 @@ SBI_2022 <- sbi_bybasin_function(date_sbi = Sys.Date(),
                                  path = "C:/Users/AJOLLYMO/RProjects/SnowData_archive/SBI_cache", # Path where you want to save a csv version of the results
                                  force = FALSE, # Force re-analysis of data rather than getting data from cache
                                  use_sbi_cache = FALSE)
+
+
+SBI_2022 <- sbi_bybasin_function(date_sbi = "01-04-2022",
+                                 all_basins = "Yes", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
+                                 exceptions = c(), # No sites to remove from analysis
+                                 incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
+                                 save_csv = "Yes", # Do you want to save
+                                 path = "C:/Users/AJOLLYMO/RProjects/SnowData_archive/SBI_cache", # Path where you want to save a csv version of the results
+                                 force = FALSE, # Force re-analysis of data rather than getting data from cache
+                                 use_sbi_cache = FALSE)
