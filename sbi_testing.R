5939e771d53796734ea97658f5754bbf7f6e53e8
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-04-14 15:40:06 -0700

    ignore

--- sbi_testing.R
+++ sbi_testing.R
@@ -5,7 +5,7 @@ library(bcsnowstats)
 library(bcsnowsbi)
 
 
-SBI_2022 <- sbi_bybasin_function(date_sbi = Sys.Date(),
+SBI_2022 <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date(),
                                  all_basins = "Yes", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
                                  exceptions = c(), # No sites to remove from analysis
                                  incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
