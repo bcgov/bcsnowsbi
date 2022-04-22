c6643d1df36e92c4778fbb8f331f5582d41f22f7
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-04-22 16:41:00 -0700

    vectorized solution to prevent the use of lapply loop (slow!)

--- R/SBI_function.R
+++ R/SBI_function.R
@@ -14,42 +14,48 @@
 #' Function for calculating the SBI once the data is collected for individual sites (output from get_SBI_data() function)
 #' @param data dataframe of sites that you are looking to calculate the SBI for
 #' @param date_sbi Date you are calculating the SBI for. Defaults to current
-#' @param basins Basins that you want to calculate SBI for
 #' @importFrom magrittr %>%
 #' @importFrom "stats" "median" "setNames"
 #' @export
 #' @keywords internal
 #' @examples \dontrun{}
 
-SBI_function <- function(data, date_sbi = Sys.Date(), basins = unique(site_basin_name()$basin)) {
+SBI_function <- function(data, date_sbi = Sys.Date()) {
 
-  #browser()
   #get the survey period for using with data
   date_sbi <- as.Date(date_sbi, format = "%d-%m-%Y")
   survey_period <- format(date_sbi, "%m-%d")
   get_year <- format(date_sbi, "%Y")
 
-  # If you haven't assigned basins as a variable, assign it as NA
-  if (!exists("basins")) {
-    basins <- NA
-  }
+  # group by basin
+  data <- data %>%
+    group_by(basin)
 
   # Get the sites from the data in
   sites_in <- data %>%
-    dplyr::filter(basin %in% basins) %>%
-    dplyr::select(id)
+    dplyr::group_by(basin) %>%
+    dplyr::select(basin, id) %>%
+    dplyr::summarize(InitialSitesused = ave(id, FUN = toString)) %>%
+    unique() %>%
+    dplyr::full_join(data %>%
+      dplyr::group_by(basin) %>%
+      dplyr::select(basin, id) %>%
+      dplyr::mutate(n_sites = length(id)) %>%
+      dplyr::select(basin, n_sites) %>%
+      unique()
+    )
 
-  sites <- sites_in$id
+  sites_initial <- data$id
 
   ## Partition by manual and ASWE sites
   # ASWE sites
-  ASWE_sites <- sites[sites %in% bcsnowdata::snow_auto_location()$LOCATION_ID]
+  ASWE_sites <- sites_initial[sites_initial %in% bcsnowdata::snow_auto_location()$LOCATION_ID]
 
   # List of manual sites
-  manual_sites <- sites[sites %in% bcsnowdata::snow_manual_location()$LOCATION_ID]
+  manual_sites <- sites_initial[sites_initial %in% bcsnowdata::snow_manual_location()$LOCATION_ID]
 
   ## Need a line or two to make sure that sites that aren't included in either manual or ASWE are still included!!
-  left_over_sites <- sites[which(!sites %in% c(manual_sites, ASWE_sites))]
+  left_over_sites <- sites_initial[which(!sites_initial %in% c(manual_sites, ASWE_sites))]
   if (length(left_over_sites) > 1) {
     sites_missing <- left_over_sites # save to the list you are creating that is output from the function
   } else {
@@ -59,13 +65,22 @@ SBI_function <- function(data, date_sbi = Sys.Date(), basins = unique(site_basin
   # Calculate the average of the SWE within all sites = average SWE/average normal SWE
   # if statement in case there is no data returned
   if (length(data) > 1) {
+
     # calculate the percent of normal from the previously calculated SWE normal
     all <- data %>%
       dplyr::mutate(percent_normal_prev = swe_mm / swenormal_prev * 100)
 
     all_1 <- all %>%
-      dplyr::ungroup() %>%
       dplyr::filter(!is.na(swe_mm)) # remove entries that do not have a current SWE measurement
+
+    # get sites with data
+    sites_data <- all_1 %>%
+      dplyr::group_by(basin) %>%
+      dplyr::select(basin, id) %>%
+      dplyr::summarize(sites_with_data = ave(id, FUN = toString)) %>%
+      unique()
+
+    sites <- dplyr::full_join(sites_in, sites_data)
 
     # ============
     # if there is a normal for the station, calculate the SBI according to the previous normals
@@ -77,20 +92,25 @@ SBI_function <- function(data, date_sbi = Sys.Date(), basins = unique(site_basin
 
     if (dim(SBI_previous_prelim)[1] > 0) {
       SBI_previous <- SBI_previous_prelim %>%
-        dplyr::mutate(SBI_new_oldnormals = round(mean(swe_mm, na.rm = TRUE)/mean(swenormal_prev, na.rm = TRUE)*100, digits = 2)) %>%
+        dplyr::mutate(SBI_new_oldnormals = round(mean(swe_mm, na.rm = TRUE) / mean(swenormal_prev, na.rm = TRUE)*100, digits = 2)) %>%
         dplyr::select(SBI_new_oldnormals) %>%
-        dplyr::distinct(SBI_new_oldnormals, .keep_all = FALSE) %>%
-        dplyr::mutate(basin = basins)
+        dplyr::distinct(SBI_new_oldnormals, .keep_all = FALSE)
 
     } else { # if there are no normals for the sites within a particular basin, assign the basin name with an NA value
-      SBI_previous <- tibble::as_tibble(basins, NaN)
+      SBI_previous <- tibble::as_tibble(unique(all_1$basins), NaN)
       colnames(SBI_previous) = c("basin", "SBI_new_oldnormals")
     }
 
     #Identify the sites used to calculate the SBI_new_oldnormals
     sites_SBI_previous <- SBI_previous_prelim %>%
-      dplyr::select(id)
-    sites_SBI_previous <- paste0(sites_SBI_previous) # identify sites that were used with SBI_new_prevnorm
+      dplyr::group_by(basin) %>%
+      dplyr::select(basin, id) %>%
+      dplyr::summarize(Sitesused_SBInew_oldnormals = ave(id, FUN = toString)) %>%
+      unique()
+
+    SBI_previous <- SBI_previous %>%
+      dplyr::full_join(sites_SBI_previous) %>%
+      dplyr::full_join(sites)
 
     if (dim(SBI_previous)[1] < 1) {
       SBI_previous[1, ] <- NaN
@@ -106,18 +126,22 @@ SBI_function <- function(data, date_sbi = Sys.Date(), basins = unique(site_basin
     if (dim(SBI_newnormals_prelim)[1] > 0) {
       SBI_newnormals <- SBI_newnormals_prelim %>%
         dplyr::mutate(SBI_newnewnormals_mean = round(mean(swe_mm, na.rm = TRUE) / mean(normal_swe_mean, na.rm = TRUE) * 100, digits = 2)) %>% # Calculate the SBI with new normals according to the mean
-        dplyr::select(SBI_newnewnormals_mean) %>%
-        dplyr::distinct(SBI_newnewnormals_mean, .keep_all = FALSE) %>%
-        dplyr::mutate(basin = basins)
+        dplyr::select(basin, SBI_newnewnormals_mean) %>%
+        dplyr::distinct(SBI_newnewnormals_mean, .keep_all = FALSE)
+
     } else { # if there are no normals for the sites within a particular basin, assign the basin name with an NA value
-      SBI_newnormals <- tibble::as_tibble(basins, NaN)
+      SBI_newnormals <- tibble::as_tibble(unique(all_1$basins), NaN)
       colnames(SBI_newnormals) <- c("basin", "SBI_newnewnormals_mean")
     }
 
     # Identify the stations used to calculate the SBI with the new normals
     sites_SBI_newnormals_mean <- SBI_newnormals_prelim %>%
-      dplyr::select(id)
-    sites_SBI_newnormals_mean <- paste0(sites_SBI_newnormals_mean) # identify sites that are used within the newSBI_newnormals calculation
+      dplyr::group_by(basin) %>%
+      dplyr::select(basin, id) %>%
+      dplyr::summarize(Sitesused_SBInew_newnormals_mean = ave(id, FUN = toString)) %>%
+      unique()
+
+    SBI_newnormals <- dplyr::full_join(SBI_newnormals, sites_SBI_newnormals_mean)
 
     if (dim(SBI_newnormals)[1] < 1) {
       SBI_newnormals[1, ] <- NaN
@@ -133,18 +157,22 @@ SBI_function <- function(data, date_sbi = Sys.Date(), basins = unique(site_basin
     if (dim(SBI_newnormals_prelim_median)[1] > 0) {
       SBI_newnormals_median <- SBI_newnormals_prelim_median %>%
         dplyr::mutate(SBI_newnewnormals_median = round(median(swe_mm, na.rm = TRUE) / median(normal_Q50, na.rm = TRUE) * 100, digits = 2)) %>% # Calculate the SBI with new normals according to the mean
-        dplyr::select(SBI_newnewnormals_median) %>%
-        dplyr::distinct(SBI_newnewnormals_median, .keep_all = FALSE) %>%
-        dplyr::mutate(basin = basins)
+        dplyr::select(basin, SBI_newnewnormals_median) %>%
+        dplyr::distinct(SBI_newnewnormals_median, .keep_all = FALSE)
+
     } else { # if there are no normals for the sites within a particular basin, assign the basin name with an NA value
-      SBI_newnormals_median <- tibble::as_tibble(basins, NaN)
+      SBI_newnormals_median <- tibble::as_tibble(unique(all_1$basins), NaN)
       colnames(SBI_newnormals_median) <- c("basin", "SBI_newnewnormals_median")
     }
 
     # Identify the stations used to calculate the SBI with the new normals
     sites_SBI_newnormals_median <- SBI_newnormals_prelim_median %>%
-      dplyr::select(id)
-    sites_SBI_newnormals_median <- paste0(sites_SBI_newnormals_median) # identify sites that are used within the newSBI_newnormals calculation
+      dplyr::group_by(basin) %>%
+      dplyr::select(basin, id) %>%
+      dplyr::summarize(Sitesused_SBInew_newnormals_median = ave(id, FUN = toString)) %>%
+      unique()
+
+    SBI_newnormals_median <- dplyr::full_join(SBI_newnormals_median, sites_SBI_newnormals_median)
 
     # Join both SBI values together
     SBI_all_1 <- dplyr::full_join(SBI_previous, SBI_newnormals)
@@ -163,25 +191,13 @@ SBI_function <- function(data, date_sbi = Sys.Date(), basins = unique(site_basin
                              dplyr::summarise(all_1, mean_percentQ50 = round(mean(percent_Q50, na.rm = TRUE), digits = 2)),
                              dplyr::summarise(all_1, mean_percentile = round(mean(percentile, na.rm = TRUE), digits = 2)),
                              dplyr::summarise(all_1, mean_percentnormal_mean = round(mean(percent_normal_mean, na.rm = TRUE), digits = 2)),
-                             dplyr::summarise(all_1, mean_percentnormal_sbiprev = round(mean(percent_normal_prev, na.rm = TRUE), digits = 2))))
+                             dplyr::summarise(all_1, mean_percentnormal_sbiprev = round(mean(percent_normal_prev, na.rm = TRUE), digits = 2)))) %>%
+      dplyr::select(-basin.1, -basin.2, -basin.3, -basin.4, -basin.5, -basin.6, -basin.7)
 
     # Bind statistics with the SBI calculation (new method) calculated using both the new and old normals
-    all_stat_SBI <- dplyr::bind_cols(all_stat, SBI_all)
+    all_stat_SBI <- dplyr::full_join(all_stat, SBI_all)
 
     all_stat_1 <- all_stat_SBI %>%
-      dplyr::mutate(n_sites = length(all_1$swe_mm)) %>%
-      dplyr::mutate(sites_with_data = list(as.matrix(unique(all_1$id)))) %>%
-      dplyr::mutate(sites_with_data = as.character(sites_with_data)) %>%
-      dplyr::mutate(InitialSitesused = as.character(list(as.matrix(unique(all$id))))) %>%
-      dplyr::mutate(Sitesused_SBInew_oldnormals = ifelse(length(sites_SBI_previous) > 0,
-                                                         as.character(list(as.matrix(sites_SBI_previous))),
-                                                         "No sites with old normals")) %>%
-      dplyr::mutate(Sitesused_SBInew_newnormals_mean = ifelse(length(sites_SBI_newnormals_mean) > 0,
-                                                              as.character(list(as.matrix(sites_SBI_newnormals_mean))),
-                                                              "No sites with new normals - mean")) %>%
-      dplyr::mutate(Sitesused_SBInew_newnormals_median = ifelse(length(sites_SBI_newnormals_median) > 0,
-                                                                as.character(list(as.matrix(sites_SBI_newnormals_median))),
-                                                                "No sites with new normals - median")) %>%
       dplyr::mutate(MissedSites = as.character(list(as.matrix(sites_missing)))) %>%
       dplyr::mutate(Survey_period = paste0(survey_period, "-", get_year))
 
