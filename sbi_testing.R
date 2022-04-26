7d2f06a67a3919a624ba930761f1f10a6b93f163
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-04-26 13:41:41 -0700

    ignore

--- sbi_testing.R
+++ sbi_testing.R
@@ -25,7 +25,7 @@ SBI_2022 <- sbi_bybasin_function(date_sbi = "01-04-2022",
                                  use_sbi_cache = FALSE)
 
 drive = "\\\\DRAIN.dmz\\Shared"
-test <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date(),
+test <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date() - 1,
                                 all_basins = "Yes", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
                                 exceptions = c(), # No sites to remove from analysis
                                 incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
