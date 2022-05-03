36ce51425215b93bda5b415f8697dd2c5de4c1da
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-05-03 11:12:11 -0700

    ignore

--- sbi_testing.R
+++ sbi_testing.R
@@ -25,7 +25,7 @@ SBI_2022 <- sbi_bybasin_function(date_sbi = "01-04-2022",
                                  use_sbi_cache = FALSE)
 
 drive = "\\\\DRAIN.dmz\\Shared"
-test <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date(),
+test <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date() - 2,
                                 all_basins = "Yes", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
                                 exceptions = c(), # No sites to remove from analysis
                                 incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
@@ -33,3 +33,10 @@ test <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date(),
                                 path = paste0(drive, "/Real-time_Data/ASP_daily_interactive/data/SBI/") # Path where you want to save a csv version of the results
                              )
 t <- test[[1]]
+ASWE_sites <- bcsnowdata::snow_auto_location()$LOCATION_ID
+data_aswe <- bcsnowstats::stats_aswe(station_id = c("2E07P", "1C05P"),
+                                     survey_period = "05-01",
+                                     get_year = "2022",
+                                     normal_min = 1991,
+                                     normal_max = 2020,
+                                     force = FALSE)
