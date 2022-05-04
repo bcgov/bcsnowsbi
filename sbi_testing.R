c51172307f664c9e36ed25c27c4af5cc5469d0d9
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-05-03 17:09:57 -0700

    ignore

--- sbi_testing.R
+++ sbi_testing.R
@@ -25,7 +25,7 @@ SBI_2022 <- sbi_bybasin_function(date_sbi = "01-04-2022",
                                  use_sbi_cache = FALSE)
 
 drive = "\\\\DRAIN.dmz\\Shared"
-test <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date() - 2,
+test <- sbi_bybasin_function(date_sbi = Sys.Date() - 2,
                                 all_basins = "Yes", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
                                 exceptions = c(), # No sites to remove from analysis
                                 incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
@@ -33,10 +33,19 @@ test <- bcsnowsbi::sbi_bybasin_function(date_sbi = Sys.Date() - 2,
                                 path = paste0(drive, "/Real-time_Data/ASP_daily_interactive/data/SBI/") # Path where you want to save a csv version of the results
                              )
 t <- test[[1]]
+
+drive = "\\\\DRAIN.dmz\\Shared"
+vancouverisland <- sbi_bybasin_function(date_sbi = Sys.Date() - 2,
+                             all_basins = "Vancouver Island", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
+                             exceptions = c(), # No sites to remove from analysis
+                             incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
+                             save_csv = "Yes", # Do you want to save
+                             path = paste0(drive, "/Real-time_Data/ASP_daily_interactive/data/SBI/") # Path where you want to save a csv version of the results
+)
+va_t <- vancouverisland[[2]]
 ASWE_sites <- bcsnowdata::snow_auto_location()$LOCATION_ID
-data_aswe <- bcsnowstats::stats_aswe(station_id = c("2E07P", "1C05P"),
-                                     survey_period = "05-01",
-                                     get_year = "2022",
-                                     normal_min = 1991,
-                                     normal_max = 2020,
-                                     force = FALSE)
+manual <- bcsnowstats::get_snow_stats(station_id = manual_sites,
+                            survey_period = time_period,
+                            get_year = get_year,
+                            normal_min = normal_min,
+                            normal_max = normal_max, force = FALSE)
