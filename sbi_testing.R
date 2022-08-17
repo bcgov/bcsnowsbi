237e7f5b84335074afa5ecf60c760f6496bba33b
Ashlee Jollymore <Ashlee.Jollymore@gov.bc.ca>
2022-08-17 16:07:03 -0700

    ignore

--- sbi_testing.R
+++ sbi_testing.R
@@ -25,7 +25,7 @@ SBI_2022 <- sbi_bybasin_function(date_sbi = "01-04-2022",
                                  use_sbi_cache = FALSE)
 
 drive = "\\\\DRAIN.dmz\\Shared"
-test <- sbi_bybasin_function(date_sbi = Sys.Date() - 2,
+test <- sbi_bybasin_function(date_sbi = Sys.Date(),
                                 all_basins = "Yes", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
                                 exceptions = c(), # No sites to remove from analysis
                                 incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
@@ -35,7 +35,7 @@ test <- sbi_bybasin_function(date_sbi = Sys.Date() - 2,
 t <- test[[1]]
 
 drive = "\\\\DRAIN.dmz\\Shared"
-vancouverisland <- sbi_bybasin_function(date_sbi = Sys.Date() - 2,
+vancouverisland <- sbi_bybasin_function(date_sbi = "2022-05-01",
                              all_basins = "Vancouver Island", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
                              exceptions = c(), # No sites to remove from analysis
                              incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
@@ -49,3 +49,25 @@ manual <- bcsnowstats::get_snow_stats(station_id = manual_sites,
                             get_year = get_year,
                             normal_min = normal_min,
                             normal_max = normal_max, force = FALSE)
+
+# Function to save historic SBI
+
+historic_sbi <- function(date) {
+  t <- sbi_bybasin_function(date_sbi = as.Date("2021-12-01"),
+                       all_basins = "Yes", # What basin to analyze SBI values for. Can also be "Yes" to get all SBI values for all basins
+                       exceptions = c(), # No sites to remove from analysis
+                       incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
+                       save_csv = "Yes", # Do you want to save
+                       path = paste0("G:/Snow/sbi_archive/"))
+}
+
+dates <- c(as.Date("2021-12-01"), as.Date("2022-01-01"), as.Date("2022-02-01"), as.Date("2022-03-01"), as.Date("2022-04-01"),
+           as.Date("2022-05-01"), as.Date("2022-05-15"), as.Date("2022-06-01"), as.Date("2022-06-15"))
+
+
+lapply(dates[2],
+       sbi_bybasin_function,
+       all_basins = "Yes",
+       incorrect_sites = c(), incorrect_data = c(), # No incorrect manual sites. Otherwise, can specify the site name and data that a manual site should be (in cases where it data was incorrectly entered).
+       save_csv = "Yes", # Do you want to save
+       path = paste0("G:/Snow/sbi_archive/"))
